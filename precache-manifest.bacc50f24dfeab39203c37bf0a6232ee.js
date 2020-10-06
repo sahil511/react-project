996296b450fc4fdc0cbe0afc74605c4d183b8240
Sahil Kharat <kharatsahil511@gmail.com>
2020-10-06 20:19:22 +0530

    Updates

---
+++ precache-manifest.bacc50f24dfeab39203c37bf0a6232ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9abe1d3bc387384be728a807108f7292",
+    "url": "/react-project/index.html"
+  },
+  {
+    "revision": "65266933df0e9cd68c1b",
+    "url": "/react-project/static/css/main.7a89181c.chunk.css"
+  },
+  {
+    "revision": "b293b0038542d9747f73",
+    "url": "/react-project/static/js/2.c9d07523.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-project/static/js/2.c9d07523.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65266933df0e9cd68c1b",
+    "url": "/react-project/static/js/main.53837779.chunk.js"
+  },
+  {
+    "revision": "ef0ba607d25339a84d10",
+    "url": "/react-project/static/js/runtime-main.ec1ffa47.js"
+  }
+]);
